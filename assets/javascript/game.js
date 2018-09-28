72a850f176c056ff0e1ed77a627e63c45b95cb08
Libby Hughes <libbyhughes@libbys-mbp.uguest.utah.edu>
2018-09-27 18:24:13 -0600

    adding files

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+.(document).ready(function() {
+
+var compPick = Math.floor(Math.random() * 102) + 19;
+    console.log("compPick: " + compPick);
+    $(".randomNumber").html(compPick);
+
+// var randomArray = [];
+// console.log("randomArray: " + randomArray);
+
+// for (i = 0; i < 4; i++) {
+// var gemNumber = Math.floor(Math.random() * 12) +1;
+//      console.log("gemNumber: " + gemNumber);
+// randomArray.push(gemNumber);
+// }
+}
+}
